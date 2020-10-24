cf2eca28b7f5f219f813198cdf7c4e63db131c52
joannaWebDev <joannasmerea@gmail.com>
2020-10-24 18:51:28 +0200

    Updates

---
+++ precache-manifest.27fc136482527f2437046add129ab41d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b89351e9d14ce232f2e74eb3690159",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "b7b95acb8b780d574a2a",
+    "url": "/Pokedex/static/css/main.3303e5f5.chunk.css"
+  },
+  {
+    "revision": "d3328476a5d2e5f33bea",
+    "url": "/Pokedex/static/js/2.495fe183.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pokedex/static/js/2.495fe183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b95acb8b780d574a2a",
+    "url": "/Pokedex/static/js/main.bfc94ee6.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
