374f71b97d9a6c8359ca3b86de6689ba9c935c66
joannaWebDev <joannasmerea@gmail.com>
2020-10-28 12:44:26 +0100

    updated week2 a to c

--- src/components/CaughtPokemon/CaughtPokemon.js
+++ src/components/CaughtPokemon/CaughtPokemon.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from "./CaughtPokemon.module.css"
 
 const CaughtPokemon = (props) => {
+    const catchPokemon = () => {
+        const [caught, setCaught] = useState(0);
+    }
+    const incrementCount = () => {
+        setCaught(caught + 1);
+      }
     /* const date  = new Date().toLocaleDateString(); */
-    return (<p>Caught 0 Pokemon on {props.date}</p>)
+    return (<>
+    <p>Caught {caught} Pokemon on {props.date}</p>
+    <button onClick={incrementCount}>Click me</button>
+    </>)
 }
 
 export default CaughtPokemon
